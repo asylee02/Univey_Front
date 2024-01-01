69c8e280801abba63f51b1147f3f3e21307b12ce
sangyeon <asylee02@gmail.com>
2024-01-01 17:47:35 +0900

    merge

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App';
+import Main from './pages/Main';
+import Board from './pages/Board';
+import Create from './pages/Create';
+import My from './pages/My';
+import Trend from './pages/Trend';
+import Search from './pages/Search';
+import Participate from './pages/Participate';
+import LoginHandle from './pages/LoginHandle';
+import QRcode from './pages/QRcode';
+import Result from './pages/Result';
+import Board2 from './pages/Board2';
+import CreateDetail from "./pages/CreateDetail";
+import ParticipateComplete from "./pages/ParticipateComplete";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Main/> },
+      { path: 'main', element: <Main /> },
+      { path: 'main/board', element: <Board2 /> },
+      { path: 'main/my', element: <My/> },
+      { path: 'main/trend', element: <Trend/> },
+      { path: 'main/search/:value', element: <Search/> },
+      { path: 'user/kakao/callback', element: <LoginHandle/>},
+      { path: 'main/create/qr', element: <QRcode/>},
+      { path: 'main/result', element: <Result/>},
+      { path: "main/create", element: <Create /> },
+      { path: "main/create/detail", element: <CreateDetail /> },
+      { path: "main/participate", element: <Participate /> },
+      { path: "main/participate/complete", element: <ParticipateComplete /> },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Trend/>
+  }
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+    <RouterProvider router={router} />
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/My.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function My() {
+  return (
+    <div className='h-screen w-full'>
+      <div className='h-1/2 border-pink-700 border-2'>
+      <  img src="https://url.kr/uy7rgj" className="  border-1 border-pink-900 h-full" />
+      </div>
+    </div>
+  );
+}
+
